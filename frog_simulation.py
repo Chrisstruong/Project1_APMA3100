0cc2edeba176cd1ae35f1b6ccb29df3cd3eceb0d
chrisstruong <chrisstruong@gmail.com>
2025-09-20 05:25:48 -0400

    Created a class for frog simulation and run it in main.py

--- frog_simulation.py
+++ frog_simulation.py
@@ -1,29 +1,50 @@
 import random
 
-"""
-remaining_distance: N value 
-total_hops: total number of jumps that the frog took
-current_pad: current position of the frog (start = 0, end = N)
-"""
+class FrogSimulation:
+    def __init__(self, N_value):
+        self.remaining_distance = N_value 
+        self.total_hops = 0 # total number of jumps that the frog took
+        self.current_pad = 0 # current position of the frog (start = 0, end = N)
 
-remaining_distance = int(input("Enter the N value : "))
-total_hops = 0
-current_pad = 0
+    def calculate_total_hops(self, N_value):
+        print("\n*****The Frog Simulation Starts****")
+        print("____________________________")
 
-print("\n*****The Frog Simulation Starts****")
-print("____________________________")
+        # The frog continues until it reaches to the other side of the pond (remaining distance = 0)
+        while (self.remaining_distance > 0):
+            self.total_hops += 1 # keep track the total number of jumps, starting at 1
+            print("Remaining distance to get to the other side of a pond", self.remaining_distance, "lily pads")
+            next_hop = random.randint(1, self.remaining_distance) # The frog picks randomly between 1-remaining_distance
+            self.current_pad += next_hop # keep track current position of the frog
+            print(f"Next hop (pick 1-{self.remaining_distance}):", next_hop)
+            self.remaining_distance -= next_hop # calculate the remaining distance
+            print("The frog is currently on lily pad number", self.current_pad)
+            print("____________________________")
 
-# The frog continues until it reaches to the other side of the pond (remaining distance = 0)
-while (remaining_distance > 0):
-    total_hops += 1 # keep track the total number of jumps, starting at 1
-    print("Remaining distance to get to the other side of a pond", remaining_distance, "lily pads")
-    next_hop = random.randint(1, remaining_distance) # The frog picks randomly between 1-remaining_distance
-    current_pad += next_hop # keep track current position of the frog
-    print(f"Next hop (pick 1-{remaining_distance}):", next_hop)
-    remaining_distance -= next_hop # calculate the remaining distance
-    print("The frog is currently on lily pad number", current_pad)
-    print("____________________________")
+        print("********Simulation Ends****\n")
+        return self.total_hops
 
-print("********Simulation Ends****\n")
-print("In this outcome, the frog took",total_hops,"total hops to complete the journey")
+
+
+
+# remaining_distance = int(input("Enter the N value : "))
+# total_hops = 0
+# current_pad = 0
+
+# print("\n*****The Frog Simulation Starts****")
+# print("____________________________")
+
+# # The frog continues until it reaches to the other side of the pond (remaining distance = 0)
+# while (remaining_distance > 0):
+#     total_hops += 1 # keep track the total number of jumps, starting at 1
+#     print("Remaining distance to get to the other side of a pond", remaining_distance, "lily pads")
+#     next_hop = random.randint(1, remaining_distance) # The frog picks randomly between 1-remaining_distance
+#     current_pad += next_hop # keep track current position of the frog
+#     print(f"Next hop (pick 1-{remaining_distance}):", next_hop)
+#     remaining_distance -= next_hop # calculate the remaining distance
+#     print("The frog is currently on lily pad number", current_pad)
+#     print("____________________________")
+
+# print("********Simulation Ends****\n")
+# print("In this outcome, the frog took",total_hops,"total hops to complete the journey")
 
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Will add the report here.")
+from frog_simulation import FrogSimulation
+
+# Part a: run the simulation
+N_value = int(input("Enter the N value: "))
+frog = FrogSimulation(N_value)
+total_hops = frog.calculate_total_hops(N_value)
+print("In this outcome, the frog took",total_hops,"total hops to complete the journey")
